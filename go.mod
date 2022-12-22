9d87f4679eb3b41a873929f0eca77ebe74406580
Athul Muralidhar <athul@ekeana.io>
2022-12-22 11:42:49 +0530

    fixed debugging stuff and added tests

--- go.mod
+++ go.mod
@@ -5,10 +5,8 @@ go 1.19
 require github.com/onsi/gomega v1.24.2
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- test/ex1_4_test.go
+++ test/ex1_4_test.go
@@ -1,6 +1,11 @@
 package test
 
-import "testing"
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
 import gomega "github.com/onsi/gomega"
 
 func TestEx1_4(t *testing.T) {
@@ -8,5 +13,30 @@ func TestEx1_4(t *testing.T) {
 
 	//f := farm.New([]string{"Cow", "Horse"})
 	//g.Expect(0).To(gomega.Equal(0))
-	
+
+}
+func setup() {
+	content1 := []byte("temporary file's content")
+	tmpfile1, err := ioutil.TempFile("./", "example.*.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(name string) {
+		err := os.Remove(name)
+		if err != nil {
+
+		}
+	}(tmpfile1.Name()) // clean up
+
+	if _, err := tmpfile1.Write(content1); err != nil {
+		err := tmpfile1.Close()
+		if err != nil {
+			return
+		}
+		log.Fatal(err)
+	}
+	if err := tmpfile1.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
