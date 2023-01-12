f860ab88ec90a945ed33b3cf066f23e4c1507aed
Athul Muralidhar <athul@ekeana.io>
2023-01-12 12:13:54 +0100

    added sorting

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "github.com/AthulMuralidhar/the-go-book/pkg/ch5"
+import (
+	"github.com/AthulMuralidhar/the-go-book/pkg/ch7"
+)
 
 func main() {
 	//ch4.GithubIssues()
 	//ch4.TextTemplate()
 	//ch4.HTMLTemplate()
-	ch5.Crawler()
+	//ch5.Crawler()
+	ch7.SortingMusic()
 }
--- pkg/ch7/sorting.go
+++ pkg/ch7/sorting.go
@@ -1,35 +1,60 @@
 package ch7
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"text/tabwriter"
+)
 
 type Track struct {
-	Title  string
-	Artist string
-	Album  string
-	Year   int
-	Length time.Duration
+	Title  string `json:"title"`
+	Artist string `json:"artist"`
+	Album  string `json:"album"`
+	Year   int    `json:"year"`
+	Length string `json:"length"`
 }
 
-func (t Track) UnmarshalJSON(bytes []byte) error {
-	//TODO implement me
-	panic("implement me")
+type TrackList struct {
+	TrackList []*Track `json:"track_list"`
 }
 
-func (t Track) String() string {
-	//TODO implement me
-	panic("implement me")
+func (tL *TrackList) Print() {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+
+	_, err := fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = fmt.Fprintf(tw, format, "-----", "-----", "-----", "-----", "-----")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, track := range tL.TrackList {
+		_, err := fmt.Fprintf(tw, format, track.Title, track.Artist, track.Album, track.Year, track.Length)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	err = tw.Flush()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-type byArtist []*Track
+func SortingMusic() {
+	var trackList TrackList
+	data, err := os.ReadFile("./data/tracks.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(data, &trackList)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func (tList byArtist) Len() int {
-	return len(tList)
-}
+	trackList.Print()
 
-func (tList byArtist) Less(i, j int) bool {
-	return tList[i].Artist < tList[j].Artist
-}
-
-func (tList byArtist) Swap(i, j int) {
-	tList[i], tList[j] = tList[j], tList[i]
 }
