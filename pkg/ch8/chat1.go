53790974a1fa8c88c4f4c41e01cad5e8800cdff5
Athul Muralidhar <athul@ekeana.io>
2023-02-03 07:53:36 +0100

    wip on error groups in the chat server
    * the problem is to make the handleconn go routine error ready

--- pkg/ch8/chat1.go
+++ pkg/ch8/chat1.go
@@ -1,13 +1,17 @@
 package ch8
 
 import (
+	"bufio"
 	"log"
 	"net"
+	"golang.org/x/sync/errgroup"
 )
 
 type client chan string
 
 func Chat1() {
+	// these are channels of chans
+	var eg errgroup.Group
 	enteringChan := make(chan client)
 	leavingChan := make(chan client)
 	messagesChan := make(chan string)
@@ -16,33 +20,53 @@ func Chat1() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	go broadcaster()
+	go broadcaster(messagesChan, enteringChan, leavingChan)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err)
 			continue
 		}
-		go handleConn(conn)
+		err := go handleConn(conn) // per client routine
 	}
 
 }
 
-func handleConn(conn net.Conn) {
+func handleConn(conn net.Conn, enteringChan, leavingChan chan client) error {
+	ch := make(chan string)
+	go clientWriter(conn, ch)
+
+	connAddr := conn.RemoteAddr().String()
+	ch <- "you are: " + connAddr
+	enteringChan <- ch
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- connAddr + ":" + input.Text()
+	}
+	leavingChan <- ch
+	messages <- connAddr + "has left"
+
+	return conn.Close()
+}
+
+func clientWriter(conn net.Conn, msgsChan chan string) {
 
 }
 
-func broadcaster(messagesChan chan string, enteringChan client) {
+func broadcaster(messagesChan chan string, enteringChan, leavingChan chan client) {
 	clients := make(map[client]bool)
 	for {
 		select {
 		case msg := <-messagesChan:
 			for c := range clients {
-				c <- msg
+				c <- msg // broadcast on a common msg channel
 			}
 		case c := <-enteringChan:
-			clients[c] = true
-		case <-leavingChan:
+			clients[c] = true // update the client set
+		case c := <-leavingChan:
+			delete(clients, c)
+			close(c) // close the client set
 
 		}
 	}
